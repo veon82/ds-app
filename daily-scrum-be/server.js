0a566b49b7080c6f6bc9cefc37570d8782b9c674
Andrea Baldini <12305563+veon82@users.noreply.github.com>
2024-04-03 10:27:13 +0200

    feat(be): request logs

--- daily-scrum-be/server.js
+++ daily-scrum-be/server.js
@@ -2,15 +2,21 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const path = require('path');
 const apiRoutes = require('./api.js');
 
 const app = express();
+app.set('trust proxy', true); // Solo se sei dietro a un proxy come Nginx o HAProxy
+
+const morganFormat = ':remote-addr - :method :url :status';
 
 app.use(express.json()); // Permette di leggere JSON dal body delle richieste
 app.use(cors({
     origin: 'http://localhost:3001'
 }));
+app.use(morgan(morganFormat));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', apiRoutes);
 
