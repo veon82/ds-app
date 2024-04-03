8af70f0da44ac7eff5e8695736e74d3ec356781b
Andrea Baldini <12305563+veon82@users.noreply.github.com>
2024-04-03 11:02:00 +0200

    fix: title and favicon.ico

--- daily-scrum-fe/src/App.js
+++ daily-scrum-fe/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
+// import RegisterPage from './components/RegisterPage';
 import SessionsPage from './components/SessionsPage';
 import DailyScrumPage from './components/DailyScrumPage';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
 import "./App.css"
